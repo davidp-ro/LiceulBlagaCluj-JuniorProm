3e3110061f5c9f75e7ce67f6072c901d57f3adaa
David Pescariu <davidpescariu12@gmail.com>
2022-10-14 10:29:04 +0300

    add: Flutter validator project

---
+++ validate_tickets/README.md
@@ -0,0 +1,3 @@
+# validate_tickets
+
+TODO
---
+++ validate_tickets/android/app/src/main/kotlin/ro/liceulblagacluj/validate_tickets/MainActivity.kt
@@ -0,0 +1,6 @@
+package ro.liceulblagacluj.validate_tickets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
