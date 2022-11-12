03fd78c752fc1f8147d0555aa5d2afe10a0667fb
David Pescariu <davidpescariu12@gmail.com>
2022-11-12 22:18:07 +0200

    fix: empty entry list bug & feat: logging

--- validate-tickets/supabase/functions/auth/index.ts
+++ validate-tickets/supabase/functions/auth/index.ts
@@ -1,23 +1,23 @@
 /**
  * @brief Handle authentication with the auth code
- * 
+ *
  * @license GPL-3.0
- * 
+ *
  * Copyright (C) 2022 <David Pescariu, @davidp-ro>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+ */
 
 import { serve } from "https://deno.land/std@0.131.0/http/server.ts";
 import { supabase, okResponse, failResponse, corsResponse } from "../lib.ts";
@@ -28,6 +28,7 @@ serve(async (req) => {
   }
 
   const { authCode } = await req.json();
+  console.log(`Authenticating with code ${authCode}`);
 
   const { data, error } = await supabase
     .from("auth")
@@ -35,10 +36,14 @@ serve(async (req) => {
     .eq("auth_code", authCode);
 
   if (error || !data || data.length == 0) {
+    console.warn(
+      `${authCode} :: Failed to authenticate w/ error: ${error ?? "Unknown"}`
+    );
     return failResponse(
       error?.message ?? "Invalid auth code / Cod de autentificare invalid"
     );
   }
 
+  console.log(`${authCode} :: Ok`);
   return okResponse(data[0].id ?? "Authenticated");
 });
--- validate-tickets/supabase/functions/validate/index.ts
+++ validate-tickets/supabase/functions/validate/index.ts
@@ -1,23 +1,23 @@
 /**
  * @brief Handle validating and updating a ticket upon scanning it
- * 
+ *
  * @license GPL-3.0
- * 
+ *
  * Copyright (C) 2022 <David Pescariu, @davidp-ro>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+ */
 
 import { serve } from "https://deno.land/std@0.131.0/http/server.ts";
 import { supabase, corsResponse, failResponse, okResponse } from "../lib.ts";
@@ -37,25 +37,36 @@ serve(async (req) => {
   }
 
   const { barcode } = await req.json();
+  console.log(`Checking code ${barcode}`);
+
   let { data, error } = await supabase
     .from("tickets")
     .select()
     .eq("code", barcode);
 
   if (error) {
+    console.warn(`${barcode} :: Failed to fetch data w/ error: ${error}`);
     return failResponse(error?.message ?? "Something went wrong");
   }
 
   if (!data || data.length == 0) {
+    console.warn(`${barcode} :: Ticket not found`);
     return failResponse("Ticket not found", 400);
   }
 
   const ticket: Ticket = data[0];
   if (ticket.entries_remaining <= 0) {
+    console.warn(`${barcode} :: Ticket has no entries remaining`);
     return failResponse("NO_ENTRY:Ticket has no entries remaining", 200);
   }
   if (ticket.expires_at < new Date()) {
+    console.warn(`${barcode} :: Ticket has expired`);
     return failResponse("NO_ENTRY:Ticket has expired", 200);
+  }
+
+  let entries = ticket.entries;
+  if (!entries) {
+    entries = [];
   }
 
   ({ data, error } = await supabase
@@ -66,14 +77,20 @@ serve(async (req) => {
       ticket_type: ticket.ticket_type,
       expires_at: ticket.expires_at,
       created_at: ticket.created_at,
-      entries: [...ticket.entries, new Date()],
+      entries: [...entries, new Date()],
     })
     .eq("code", barcode)
     .select());
 
   if (error) {
+    console.warn(`${barcode} :: Failed to update w/ error: ${error}`);
     return failResponse(error?.message ?? "Something went wrong");
   }
 
+  console.log(
+    `${barcode} :: Ticket is ok. Entries remaining: ${
+      ticket.entries_remaining - 1
+    }`
+  );
   return okResponse(data);
 });
